8e088a6a623c8f26c37df3bf852d0f07805132a8
Be-Asia McKerracher <amerigirl29@gmail.com>
2025-02-06 18:01:45 -0800

    looking at different sizes with buttons

--- src/Components/MuiButton.tsx
+++ src/Components/MuiButton.tsx
@@ -12,7 +12,7 @@ const MuiButton = () => {
         <Button variant="outlined">Outlined</Button>
       </Stack>
 
-      <Stack spacing={2} direction="row">
+      <Stack className="App" spacing={2} direction="row">
         <Button variant="contained" color="primary">
           Primary
         </Button>
@@ -27,6 +27,52 @@ const MuiButton = () => {
         </Button>
         <Button variant="contained" color="success">
           Success
+        </Button>
+      </Stack>
+      <Stack className="App" spacing={2} direction="row">
+        <Button variant="text" color="primary">
+          Primary
+        </Button>
+        <Button variant="text" color="secondary">
+          Secondary
+        </Button>
+        <Button variant="text" color="error">
+          Error
+        </Button>
+        <Button variant="text" color="warning">
+          Warning
+        </Button>
+        <Button variant="text" color="success">
+          Success
+        </Button>
+      </Stack>
+      <Stack className="App" spacing={2} direction="row">
+        <Button variant="outlined" color="primary">
+          Primary
+        </Button>
+        <Button variant="outlined" color="secondary">
+          Secondary
+        </Button>
+        <Button variant="outlined" color="error">
+          Error
+        </Button>
+        <Button variant="outlined" color="warning">
+          Warning
+        </Button>
+        <Button variant="outlined" color="success">
+          Success
+        </Button>
+      </Stack>
+
+      <Stack className="App" display="block" spacing={2} direction="row">
+        <Button variant="contained" size="small">
+          Small
+        </Button>
+        <Button variant="contained" size="medium">
+          Medium
+        </Button>
+        <Button variant="contained" size="large">
+          Large
         </Button>
       </Stack>
     </Stack>
