f0f0fbc31ed5a02281d968adcb730e9051b9bb51
Be-Asia McKerracher <amerigirl29@gmail.com>
2025-02-06 17:39:54 -0800

    gutter bottom and variants

--- src/Components/MuiTypography.tsx
+++ src/Components/MuiTypography.tsx
@@ -7,15 +7,26 @@ const MuiTypography = () => {
       <Typography variant="h1">h1 Heading</Typography>
       <Typography variant="h2">h2 Heading</Typography>
       <Typography variant="h3">h3 Heading</Typography>
-      <Typography variant="h4">h4 Heading</Typography>
+      <Typography variant="h4" component='h1' gutterBottom>h4 Heading</Typography> 
+      {/* gutter bottom adds a bit of space at the bottom */}
       <Typography variant="h5">h5 Heading</Typography>
       <Typography variant="h6">h6 Heading</Typography>
       <Typography variant="subtitle1">subtitle1</Typography>
       <Typography variant="subtitle2">subtitle2</Typography>
       <Typography variant="subtitle2">subtitle2</Typography>
 
-      <Typography>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis magnam dicta rerum odio, excepturi ea accusantium sequi soluta dolore, laborum perspiciatis totam expedita tenetur veniam possimus, quidem delectus dolorum blanditiis?</Typography>
-      <Typography variant="body2">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestiae dignissimos temporibus velit similique, aliquam, modi culpa officia dolores nisi, debitis dolore fuga maxime magnam cumque tenetur incidunt atque ullam ab.</Typography>
+      <Typography>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis magnam
+        dicta rerum odio, excepturi ea accusantium sequi soluta dolore, laborum
+        perspiciatis totam expedita tenetur veniam possimus, quidem delectus
+        dolorum blanditiis?
+      </Typography>
+      <Typography variant="body2">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestiae
+        dignissimos temporibus velit similique, aliquam, modi culpa officia
+        dolores nisi, debitis dolore fuga maxime magnam cumque tenetur incidunt
+        atque ullam ab.
+      </Typography>
     </div>
   );
 };
