df18d9bd096682aab7fa5aef04c57067e990b04d
Be-Asia McKerracher <amerigirl29@gmail.com>
2025-02-06 17:33:19 -0800

    working with variants

--- src/Components/MuiTypography.tsx
+++ src/Components/MuiTypography.tsx
@@ -10,6 +10,12 @@ const MuiTypography = () => {
       <Typography variant="h4">h4 Heading</Typography>
       <Typography variant="h5">h5 Heading</Typography>
       <Typography variant="h6">h6 Heading</Typography>
+      <Typography variant="subtitle1">subtitle1</Typography>
+      <Typography variant="subtitle2">subtitle2</Typography>
+      <Typography variant="subtitle2">subtitle2</Typography>
+
+      <Typography>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis magnam dicta rerum odio, excepturi ea accusantium sequi soluta dolore, laborum perspiciatis totam expedita tenetur veniam possimus, quidem delectus dolorum blanditiis?</Typography>
+      <Typography variant="body2">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestiae dignissimos temporibus velit similique, aliquam, modi culpa officia dolores nisi, debitis dolore fuga maxime magnam cumque tenetur incidunt atque ullam ab.</Typography>
     </div>
   );
 };
